4eb68912f94189cc111d9cbbf2d2811c10c7ef3b
Jaehee Yun <jaeheeyun@Jaeheeui-MacBookAir.local>
2021-02-14 14:32:57 +0900

    timer

---
+++ timer.js
@@ -0,0 +1,69 @@
+const timerContainer = document.querySelector(".js-timer"),
+  timerMin = timerContainer.querySelector(".timer-min"),
+  timerSec = timerContainer.querySelector(".timer-sec"),
+  timerBtn = timerContainer.querySelector(".js-timer-start"),
+  timerMinPlace = document.getElementsByName("timer-min"),
+  timerSecPlace = document.getElementsByName("timer-sec");
+
+let timer_min;
+let timer_sec;
+let timerVar;
+
+function stopTimer() {
+  clearInterval(timerVar);
+  timerBtn.addEventListener("click", startTimer);
+  timerBtn.innerText = ("시작!");
+}
+function startTimer() {
+  timerVar = setInterval(getTimer, 1000);
+  console.log("start");
+  timerBtn.removeEventListener("click", startTimer);
+  timerBtn.addEventListener("click", stopTimer);
+  timerBtn.innerText = ("정지!");
+}
+
+function getTimer() {
+
+  timer_min = parseInt(timerMin.value);
+  timer_sec = parseInt(timerSec.value);
+
+  /* timer 값을 못 받아 왔을 때 0으로 바꿔 준다. */
+  if (!timer_min) {
+    console.log(timer_min);
+    timer_min = 0;
+  }
+  if (!timer_sec) {
+    timer_sec = 0;
+  }
+
+
+  if (timer_sec != 0) {
+    timer_sec = (timer_sec - 1);
+
+    /** 타이머  */
+    timerSecPlace[0].value = `${timer_sec}`;
+
+
+    if (timer_sec == 0 && timer_min == 0) {
+      alert("Time OVER!!!!");
+      clearInterval(timerVar);
+    }
+
+  } else if (timer_sec == 0) {
+    if (timer_min == 0) {
+      alert("Time OVER!!!!");
+      clearInterval(timerVar);
+    } else {
+      timer_min = timer_min - 1;
+      timer_sec = timer_sec = 59;
+      timerSecPlace[0].value = `${timer_sec}`;
+      timerMinPlace[0].value = `${timer_min}`;
+    }
+  }
+
+}
+function init() {
+
+  timerBtn.addEventListener("click", startTimer);
+}
+init();
--- todo.js
+++ todo.js
@@ -3,8 +3,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   todoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
+const SUBTODOS_LS = "subToDos"
 let toDos = [];
+let subToDos = [];
 
+function paintSubToDos() {
+
+}
+function addEvent() {
+  const addBtn = document.getElementsByClassName(".addBtnClass");
+  console.log("add")
+  addBtn.addEventListener("submit", paintSubToDos)
+}
+function addToDo() {
+  console.log("add");
+  const ls = document.createElement("ls");
+  const inputBox = document.createElement("input");
+
+  inputBox.placeholder = "input here";
+  inputBox.className = "addBox";
+  const addBox = document.querySelector("addBox");
+  console.log(addBox);
+  ls.appendChild(inputBox);
+  todoList.appendChild(ls);
+
+  addEvent();
+
+}
 function filterFn(toDo) {
   return toDo.id === 1;
 }
@@ -21,15 +46,23 @@ function deleteToDo(event) {
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
+function saveSubToDos() {
+  localStorage.setItem(SUBTODOS_LS, JSON.stringify(subToDos));
+}
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("Button");
+  const addBtn = document.createElement("Button");
+  addBtn.className = "addBtnClass";
   const span = document.createElement("span");
   const newId = toDos.length + 1;
+  addBtn.innerText = "➡️";
+  addBtn.addEventListener("click", addToDo);
   delBtn.innerText = "×";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
+  li.appendChild(addBtn);
   li.appendChild(delBtn);
   li.id = newId;
   todoList.appendChild(li);
