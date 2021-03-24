7f651b9a28bf071061fe9a5f1ca0bd41fa4c9d30
mothy900 <mothy900@naver.com>
2021-03-24 16:04:03 +0900

    image update

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ function paintImage(imgNumber) {
   const image = new Image();
   //image.src = `images/${imgNumber + 1}.jpg`;
   //image.src = `images/7.jpeg`;
-  image.src = `https://i.pinimg.com/originals/ce/d6/36/ced636d0c6ded839fab4f993ef174dfb.jpg`;
+  image.src = `https://images.app.goo.gl/3hAeNJtHGzSVQcFr8`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
