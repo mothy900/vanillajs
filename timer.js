2d5e3011a7e043beadf1d7b6956a1c79adbf53d2
mothy900 <mothy900@naver.com>
2021-05-13 23:56:42 +0900

    fix bug

--- timer.js
+++ timer.js
@@ -14,11 +14,9 @@ function timerHide() {
   timerContainer.style.visibility = "hidden";
   timerIcon.removeEventListener("click", timerHide);
   timerIcon.addEventListener("click", timerShow);
-
 }
 
 function timerShow() {
-
   timerContainer.style.visibility = "visible";
   timerIcon.removeEventListener("click", timerShow);
   timerIcon.addEventListener("click", timerHide);
@@ -27,18 +25,17 @@ function timerShow() {
 function stopTimer() {
   clearInterval(timerVar);
   timerBtn.addEventListener("click", startTimer);
-  timerBtn.innerText = ("시작!");
+  timerBtn.innerText = "시작!";
 }
 function startTimer() {
   timerVar = setInterval(getTimer, 1000);
   console.log("start");
   timerBtn.removeEventListener("click", startTimer);
   timerBtn.addEventListener("click", stopTimer);
-  timerBtn.innerText = ("정지!");
+  timerBtn.innerText = "정지!";
 }
 
 function getTimer() {
-
   timer_min = parseInt(timerMin.value);
   timer_sec = parseInt(timerSec.value);
 
@@ -51,19 +48,17 @@ function getTimer() {
     timer_sec = 0;
   }
 
-
   if (timer_sec != 0) {
-    timer_sec = (timer_sec - 1);
+    timer_sec = timer_sec - 1;
 
     /** 타이머  */
     timerSecPlace[0].value = `${timer_sec}`;
 
-
     if (timer_sec == 0 && timer_min == 0) {
       alert("Time OVER!!!!");
       clearInterval(timerVar);
+      stopTimer();
     }
-
   } else if (timer_sec == 0) {
     if (timer_min == 0) {
       window.alert("Time OVER!!!!");
@@ -75,7 +70,6 @@ function getTimer() {
       timerMinPlace[0].value = `${timer_min}`;
     }
   }
-
 }
 function init() {
   timerBtn.addEventListener("click", startTimer);
